7deee829ae2fc4c5cd2b87d761fe743e21d6dcc4
Outwater <kocon135@gmail.com>
2022-01-03 15:46:15 +0900

    웹컴포넌트만들기_이벤트버블링 추가삭제 done

--- 과제/VanillaJs/웹컴포넌트만들기/src/components/Items.js
+++ 과제/VanillaJs/웹컴포넌트만들기/src/components/Items.js
@@ -1,23 +1,40 @@
 import Component from "../core/Component.js";
-
 export default class Items extends Component {
   setup() {
     this.$state = { items: ["item1", "item2"] };
   }
   template() {
+    //Todo1: 삭제기능구현
     const { items } = this.$state;
     return `
       <ul>
-      ${items.map((item) => `<li>${item}</li>`).join("")}
+      ${items
+        .map(
+          (item, key) => `
+        <li>
+            ${item}
+            <button class='deleteBtn' data-index="${key}">삭제</button>
+        </li>
+        `
+        )
+        .join("")}
       </ul>
-      <button>추가</button>
+      <button class="addBtn">추가</button>
       `;
   }
-
   setEvent() {
-    this.$target.querySelector("button").addEventListener("click", () => {
+    // ToDo2: 이벤트 버블링을 이용하여 이벤트 처리하기
+    this.$target.addEventListener("click", ({ target }) => {
       const { items } = this.$state;
-      this.setState({ items: [...items, `item${items.length + 1}`] });
+
+      if (target.classList.contains("addBtn")) {
+        this.setState({ items: [...items, `item${items.length + 1}`] });
+      }
+
+      if (target.classList.contains("deleteBtn")) {
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      }
     });
   }
 }
