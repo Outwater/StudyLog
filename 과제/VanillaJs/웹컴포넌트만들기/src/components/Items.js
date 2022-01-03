c77237596e99fa251316d8c00b888b70459a0c5b
Outwater <kocon135@gmail.com>
2022-01-03 17:08:16 +0900

    웹컴포넌트만들기_이벤트버블링 추상화 done

--- 과제/VanillaJs/웹컴포넌트만들기/src/components/Items.js
+++ 과제/VanillaJs/웹컴포넌트만들기/src/components/Items.js
@@ -4,7 +4,6 @@ export default class Items extends Component {
     this.$state = { items: ["item1", "item2"] };
   }
   template() {
-    //Todo1: 삭제기능구현
     const { items } = this.$state;
     return `
       <ul>
@@ -23,18 +22,15 @@ export default class Items extends Component {
       `;
   }
   setEvent() {
-    // ToDo2: 이벤트 버블링을 이용하여 이벤트 처리하기
-    this.$target.addEventListener("click", ({ target }) => {
+    // ToDo3-2. 이벤트버블링 추상화
+    this.addEvent("click", ".addBtn", ({ target }) => {
       const { items } = this.$state;
-
-      if (target.classList.contains("addBtn")) {
-        this.setState({ items: [...items, `item${items.length + 1}`] });
-      }
-
-      if (target.classList.contains("deleteBtn")) {
-        items.splice(target.dataset.index, 1);
-        this.setState({ items });
-      }
+      this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+    this.addEvent("click", ".deleteBtn", ({ target }) => {
+      const { items } = this.$state;
+      items.splice(target.dataset.index, 1);
+      this.setState({ items });
     });
   }
 }
--- 과제/VanillaJs/웹컴포넌트만들기/src/core/Component.js
+++ 과제/VanillaJs/웹컴포넌트만들기/src/core/Component.js
@@ -19,4 +19,20 @@ export default class Component {
     this.$state = { ...this.$state, ...newState };
     this.render();
   }
+
+  //ToDo3. 이벤트버블링 추상화
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+    // 하위의 모든 해당 셀렉터의 요소들을 가져온다.
+    // selector에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐 closest를 이용하여 처리한다.
+
+    const isTarget = (target) => {
+      return children.includes(target) || target.closest(selector);
+    };
+
+    this.$target.addEventListener(eventType, (event) => {
+      if (!isTarget(event.target)) return false;
+      callback(event);
+    });
+  }
 }
